9ff5c295402ae519a74c417b94457d71d4de5022
mahracha02 <abdelhakim22mahracha@gmail.com>
2024-12-10 15:55:26 +0100

    fix : test run correctly

--- test.js
+++ test.js
@@ -5,6 +5,5 @@ if (valeur1 > valeur2) {
   console.log('valeur1 est plus grand que valeur2');
   
 } else {
-  console.log('valeur2 est plus grand que valeur1');
+  throw new Error('valeur1 est plus grand que valeur2');
 } 
-throw new Error('valeur1 est plus grand que valeur2');
