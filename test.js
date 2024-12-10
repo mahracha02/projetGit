d9bc8ba507884e7787e0dad1515cad9a5775b68a
mahracha02 <abdelhakim22mahracha@gmail.com>
2024-12-10 15:44:10 +0100

    ok

---
+++ test.js
@@ -0,0 +1,10 @@
+const valeur1 = 42;
+const valeur2 = 32;
+
+if (valeur1 > valeur2) {
+  console.log('valeur1 est plus grand que valeur2');
+  
+} else {
+  console.log('valeur2 est plus grand que valeur1');
+} 
+throw new Error('valeur1 est plus grand que valeur2');
